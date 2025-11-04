e1b5742f6593801204bade8886190f0d4afb70eb
parsaT79 <parsa.taraj@gmail.com>
2025-11-04 13:32:51 +0300

    ParsaT79 tested database, successful

---
+++ db/db_test.sql
@@ -0,0 +1,5 @@
+--insert or ignore into cars (id, car_id, brand, model, year, price) values
+--(1, 123456, 'benz', '190e', 1996, 200000)
+
+--insert or ignore into customers (id, customer_id, first_name, last_name, mobile_number, address) values
+--(1, 123456, 'ali', 'rezaii', 09123456070, 'street, number')
